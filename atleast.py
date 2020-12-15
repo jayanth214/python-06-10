39ff9fa84f9a3fe7c46cfe387359e27a91c57be9
jayanth214 <70198752+jayanth214@users.noreply.github.com>
2020-12-15 09:52:13 +0530

    Add files via upload

---
+++ atleast.py
@@ -0,0 +1,12 @@
+a=int(input())
+10
+b=int(input())
+5
+c=int(input())
+3
+if(a<=b) and (a<=c):
+print("smallest is:"a)
+elif(b<=a) and(b<=c):
+print("smallest is:",b)
+else:
+print("smallest is:",c)
